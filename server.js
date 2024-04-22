975b7f1f338105c71714b1fcf2510a3500333f96
tndavis <97129436+tndavis@users.noreply.github.com>
2024-04-22 16:19:23 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
-const corsOptions = require('./config/corsOptions');
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const mongoose = require('mongoose');
@@ -17,8 +16,7 @@ connectDB();
 app.use(logger);
 
 // Cross Origin Resource Sharing
-app.use(cors(corsOptions));
-
+app.use(cors())
 // built-in middleware to handle urlencoded data
 // in other words, form data:  
 app.use(express.urlencoded({ extended: false }));
